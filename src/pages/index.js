19473402ad050e5fc35a4b5010dfdd875a79df40
Mike Van <miike.vaan@gmail.com>
2019-04-25 17:35:38 +0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>Welcome to React-Static</h1>
+    <button className="p:15px-30px br:5px bs:1 bd:0 bg:prim c:white m:10px fl:r">FUCK U CSS</button>
+  </div>
+)
---
+++ static.config.js
@@ -0,0 +1,57 @@
+import path from 'path'
+import axios from 'axios'
+import React, {Component} from 'react'
+
+export default {
+  getRoutes: async () => {
+    const { data: posts } = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts'
+    )
+
+    return [
+      {
+        path: '/blog',
+        getData: () => ({
+          posts,
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          template: 'src/containers/Post',
+          getData: () => ({
+            post,
+          }),
+        })),
+      },
+    ]
+  },
+  plugins: [
+    [
+      require.resolve('react-static-plugin-source-filesystem'),
+      {
+        location: path.resolve('./src/pages'),
+      },
+    ],
+    require.resolve('react-static-plugin-reach-router'),
+    require.resolve('react-static-plugin-sitemap'),
+  ],
+  devServer: {
+    port: 3000,
+    host: '127.0.0.1',
+  },
+  Document: ({
+    Html,
+    Head,
+    Body,
+    children,
+    state: { siteData, renderMeta },
+  }) => (
+    <Html lang="en-US">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script src="https://cdn.jsdelivr.net/npm/fucss@0.8.5" ></script>
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  ),
+}
