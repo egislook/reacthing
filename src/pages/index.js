8fe578206337fe12eac18fc693bac6e27752411b
Egis <lukas.egidijus@gmail.com>
2019-05-17 06:25:33 +0000

    bumped next-fucss to support style generator better. Configs

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default () => {
     <div style={{ textAlign: 'center' }}>
       <h1 className="p:10px fs:230pc">Welcome to React-Statics</h1>
       <div>This is Fucss working example. Dont forget to use <strong>cssReload();</strong> for hot fucss reload</div>
-      <button className="p:15px-30px br:5px bs:1 bd:0 bg:prim c:white m-t:20px">FUCSS POWER</button>
+      <button className="p:15px-30px br:5px bs:1 bd:0 bg:prim c:white m-t:30px">FUCSS POWER</button>
     </div>
   )
 }
--- static.config.js
+++ static.config.js
@@ -39,6 +39,7 @@ export default {
   devServer: {
     port: '8080',
     host: '0.0.0.0',
-    disableHostCheck: true
+    socket: 'socket',
+    disableHostCheck: true,
   }
 }
